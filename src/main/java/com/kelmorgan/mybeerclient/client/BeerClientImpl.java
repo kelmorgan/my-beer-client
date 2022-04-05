524f99ad0e8b89b5f14b904bde4b7a9ad19bd7fa
Kel Morgan <kelmorgan18@gmail.com>
2022-04-05 19:21:34 +0100

    Added Service annotation

--- src/main/java/com/kelmorgan/mybeerclient/client/BeerClientImpl.java
+++ src/main/java/com/kelmorgan/mybeerclient/client/BeerClientImpl.java
@@ -2,11 +2,15 @@ package com.kelmorgan.mybeerclient.client;
 
 import com.kelmorgan.mybeerclient.model.BeerDto;
 import com.kelmorgan.mybeerclient.model.BeerPageList;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
     @Override
     public Mono<BeerDto> getBeerById(UUID id, Boolean showInventoryOnHand) {
